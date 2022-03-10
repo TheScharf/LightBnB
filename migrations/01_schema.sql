e151e5221135d8333ab8ac2ffaf85d7190c5b7fe
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-03-09 20:50:47 -0700

    adding seeds and info into the tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE users (
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  
+
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -48,3 +48,4 @@ CREATE TABLE property_reviews (
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
+
